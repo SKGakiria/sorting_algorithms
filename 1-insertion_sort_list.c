60b8372a1b4519812b49264340b332c070fd8b0b
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-25 21:42:41 +0300

    Update remote repo

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,11 +1,10 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 void insert_node(listint_t **list, listint_t *j, listint_t *i);
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *i, *j;
+	int swapped;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
@@ -14,21 +13,25 @@ void insertion_sort_list(listint_t **list)
 	while (j)
 	{
 		i = *list;
+		swapped = 0;
 		while (1)
 		{
 			if (i == j)
+			{
+/*				print_list(*list);*/
 				break;
-			if (i == NULL)
-				printf("i is NULL\n"), exit(1);
-			if (j == NULL)
-				printf("j is NULL\n"), exit(1);
+			}
 			if (i->n > j->n)
 			{
 				insert_node(list, j, i);
-				print_list(*list);
+				swapped = 1;
+/*				print_list(*list);*/
+				break;
 			}
 			i = i->next;
 		}
+		if (swapped)
+			print_list(*list);
 		j = j->next;
 	}
 }
