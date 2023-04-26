f54e263bc84b8320fef6afb9a6c6586917eb3f62
SKGakiria <swgakiria@gmail.com>
2023-04-26 13:26:36 -0700

    updated merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -7,6 +7,13 @@ void td_split_merge(int *b, size_t start, size_t end, int *array);
 void td_merge(int *array, size_t start, size_t mid, size_t end, int *b);
 void copy_array(int *array, size_t start, size_t end, int *b);
 
+/**
+* merge_sort - sorts an array of integers in ascending order
+* @array: array to be sorted
+* @size: size of array
+*
+* Return: void
+*/
 void merge_sort(int *array, size_t size)
 {
 	int *b;
@@ -22,12 +29,29 @@ void merge_sort(int *array, size_t size)
 	free(b);
 }
 
+/**
+* td_mergesort - merge sorting two arrays
+* @array: array to be sorted
+* @b: pointer to buffer to hold a copy of @array
+* @size: size of array
+*
+* Return: void
+*/
 void td_mergesort(int *array, int *b, size_t size)
 {
 	copy_array(array, 0, size, b);
 	td_split_merge(b, 0, size, array);
 }
 
+/**
+* td_split_merge - recursively splits an unsorted array
+* @b: pointer to a buffer with a copy of array
+* @start: start point of array
+* @end: size of array
+* @array: pointer to aaray
+*
+* Return: void
+*/
 void td_split_merge(int *b, size_t start, size_t end, int *array)
 {
 	size_t mid;
@@ -42,6 +66,16 @@ void td_split_merge(int *b, size_t start, size_t end, int *array)
 	td_merge(b, start, mid, end, array);
 }
 
+/**
+* td_merge - merges two arrays
+* @array: pointer to array
+* @start: start of left side of array
+* @mid: start of right side of array
+* @end: size of array
+* @b: pointer to buffer into which merging is done
+*
+* Return: void
+*/
 void td_merge(int *array, size_t start, size_t mid, size_t end, int *b)
 {
 	size_t i = start, j = mid, k;
@@ -69,6 +103,15 @@ void td_merge(int *array, size_t start, size_t mid, size_t end, int *b)
 	print_array(b + start, end - start);
 }
 
+/**
+* copy_array - copy from array into b
+* @array: pointer to array
+* @start: start of array
+* @end: size of array
+* @b: buffer to hold a copy of array
+*
+* Return: void
+*/
 void copy_array(int *array, size_t start, size_t end, int *b)
 {
 	size_t k;
