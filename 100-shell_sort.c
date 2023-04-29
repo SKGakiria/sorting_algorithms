c52a65b1b1cf4b2a4988db54682379457f03bf54
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-29 23:01:15 +0300

    Update remote

---
+++ 100-shell_sort.c
@@ -0,0 +1,87 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t *knuth_seq(size_t size);
+
+/**
+ * shell_sort - Sort an array of integers in ascending order using
+ * the insertion sort algorithm.
+ * @array: Pointer to the array to be sorted
+ * @size: Size of array pointed to by @array
+ *
+ * Return: void.
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t i, j, *gaps;
+	int temp, gap_idx, k;
+
+	if (array == NULL || size < 2)
+		return;
+
+	gaps = knuth_seq(size);
+	if (gaps == NULL)
+		return;
+/*
+	printf("Knuth sequence: [");
+	for (k = 0; gaps[k]; k++)
+		printf("%lu ", gaps[k]);
+	puts("]");
+
+*/	for (gap_idx = 0; gaps[gap_idx]; gap_idx++)
+	{
+		for (i = gaps[gap_idx]; i < size; i++)
+		{
+			temp = array[i];
+
+			for (j = i; (j >= gaps[gap_idx]) && (array[j - gaps[gap_idx]] > temp); j--)
+			{
+				array[j] = array[j - gaps[gap_idx]];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+
+	free(gaps);
+}
+
+/**
+ * knuth_seq - generate a knuth sequence
+ * @size: upper limit on the largest number in the sequence
+ *
+ * Return: pointer to the knuth sequence, or NULL (failure).
+ */
+size_t *knuth_seq(size_t size)
+{
+	size_t i = 0, j = 1, k = 1, gap = 0, x;
+	size_t *gaps, temp;
+
+	while (k < size)
+	{
+		k = k * 3 + 1;
+		i++;
+	}
+
+	gaps = malloc((i + 1) * sizeof(size_t));
+	if (gaps == NULL)
+		return (NULL);
+
+	gaps[0] = 0;
+	while (j < i)
+	{
+		gap = gap * 3 + 1;
+		gaps[j] = gap;
+		j++;
+	}
+
+	for (x = 0; x < (i + 1) / 2; x++)
+	{
+		temp = gaps[x];
+		gaps[x] = gaps[i - x];
+		gaps[i - x] = temp;
+	}
+
+	return (gaps);
+}
--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,5 +1,12 @@
 #include "sort.h"
 
+/**
+ * swap - swap two integers
+ * @a: pointer to integer
+ * @b: pointer to integer
+ *
+ * Return: void.
+ */
 void swap(int *a, int *b)
 {
 	int temp = *a;
@@ -7,39 +14,59 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
+/**
+ * mx_heapify - build a max-heap from an array
+ * @array: pointer to an array of integers
+ * @size: length of buffer pointed to by @array
+ * @i: initial value with which to set the largest index
+ *
+ * Return: void.
+ */
 void mx_heapify(int *array, size_t size, size_t i)
 {
 	size_t largest = i;
-	size_t l = 2 * i;
-	size_t r = (2 * i) + 1;
+	size_t l = 2 * i + 1;
+	size_t r = (2 * i) + 2;
 
-	while(l <= size && array[l] > array[largest])
+	if (l < size && array[l] > array[largest])
 	{
 		largest = l;
 	}
-	while(r <= size && array[r] > array[largest])
+
+	if (r < size && array[r] > array[largest])
 	{
 		largest = r;
 	}
-	if(largest != i)
+
+	if (largest != i)
 	{
-		swap(&array[largest], &array[i]);
+		swap(&(array[largest]), &(array[i]));
 		print_array(array, size);
 		mx_heapify(array, size, largest);
 	}
 }
 
+/**
+ * heap_sort - sort an array of integers in ascending order using
+ * the heap sort algorithm
+ * @array: pointer to array of integers
+ * @size: size of array
+ *
+ * Return: void.
+ */
 void heap_sort(int *array, size_t size)
 {
-	size_t i;
-	for(i = size / 2; i >= 1; i--)
+	int i;
+
+	for (i = size / 2 - 1; i >= 0; i--)
 	{
 		mx_heapify(array, size, i);
 	}
-	for(i = size; i >= 1; i--)
+
+	for (i = size - 1; i >= 0; i--)
 	{
-		swap(&array[1], &array[i]);
+		swap(&array[0], &(array[i]));
 		print_array(array, size);
-		mx_heapify(array, size, 1);
+		mx_heapify(array, i, 0);
 	}
 }
